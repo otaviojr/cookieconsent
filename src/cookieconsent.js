3ba9a8f01c848202b08f169da8548bcc63f26cb1
Otávio Ribeiro <otavio.ribeiro@gmail.com>
2018-05-25 13:52:59 -0300

    Law initialise bugfix

--- src/cookieconsent.js
+++ src/cookieconsent.js
@@ -1412,7 +1412,7 @@
     };
 
     Law.prototype.applyLaw = function(options, countryCode) {
-      var complete = this.options.onInitialise.bind(this);
+      var complete = options.onInitialise.bind(this);
       var country = this.get(countryCode);
 
       if (!country.hasLaw) {
